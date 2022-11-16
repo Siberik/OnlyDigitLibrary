6cd2ef22f8409405eb7fd082d567558998665b9c
Сын <iamvladgubarevofficial@gmail.com>
2022-11-16 21:47:50 +0500

    Работа над преобразованием int в char

--- OnlyDigitLibrary/Class1.cs
+++ OnlyDigitLibrary/Class1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,16 @@ namespace OnlyDigitLibrary
 {
     public class Class1
     {
+        public static List<char> GetDigit(string textString)
+        {
+            var text = textString.Split(' ');
+            var text2 = Array.ConvertAll(text, s => int.Parse(s));
+            text2 = text2.Where(x => x > 5).ToArray();
+           List<int> arr=text2.OfType<int>().ToList();
+            arr.Distinct().OrderBy(x => x);
+
+            
+
+        }
     }
 }
