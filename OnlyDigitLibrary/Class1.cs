88a7e5cc378affd3438eb3ff4f876d865054925a
Сын <iamvladgubarevofficial@gmail.com>
2022-11-16 22:02:59 +0500

    Сделана предварительная версия библиотеки и добавлены комментарии

--- OnlyDigitLibrary/Class1.cs
+++ OnlyDigitLibrary/Class1.cs
@@ -9,13 +9,26 @@ namespace OnlyDigitLibrary
 {
     public class Class1
     {
+        /// <summary>
+        /// Возвращение списка всех цифр, подходящих условиям
+        /// </summary>
+        /// <param name="textString">
+        /// Текстовая переменная со всеми цифрами
+        /// </param>
+        /// <returns>
+        /// Возвращает отсортированный список
+        /// </returns>
         public static List<char> GetDigit(string textString)
         {
             var text = textString.Split(' ');
             var text2 = Array.ConvertAll(text, s => int.Parse(s));
             text2 = text2.Where(x => x > 5).ToArray();
-           List<int> arr=text2.OfType<int>().ToList();
-            arr.Distinct().OrderBy(x => x);
+           var arr=text2.OfType<int>().ToList();
+            var arr2=arr.OfType<char>().ToList();
+            List<char> result=new List<char>(arr2);
+           var res= result.Distinct().OrderBy(x => x).ToList();
+            return res;
+            
 
             
 
